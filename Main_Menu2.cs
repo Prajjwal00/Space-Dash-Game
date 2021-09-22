dc2e32af1b9f7263447eb5989b42a86b7b6df854
Prajjwal00 <68287383+Prajjwal00@users.noreply.github.com>
2021-09-22 13:58:31 +0530

    Add files via upload

---
+++ Main_Menu2.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main_Menu2 : MonoBehaviour
+{
+
+  public void LoadSinglePlayerGame()
+
+    {
+      Debug.Log("Single Player Game Loading....");
+     SceneManager.LoadScene(1);
+
+    }
+    public void LoadCoOpMode()
+    {
+        Debug.Log(" Co-op Game Loading....");
+        SceneManager.LoadScene(2);
+    }
+}
